6c49b9a031e7c1c5e3c95b40811a3f34c650666f
juan1t0 <jherediaparillo@gmail.com>
2025-05-21 09:55:02 -0300

    correct the repo name

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -88,7 +88,7 @@
             let {author, date, time, timezone, datetime} = first;
             let ret = {
                 id: commit,
-                url: "https://github.com/maurodiaz7/my-portfolio/commit/" + commit,
+                url: "https://github.com/juan1t0/my-portfolio/commit/" + commit,
                 author, date, time, timezone, datetime,
                 hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
                 totalLines: lines.length
